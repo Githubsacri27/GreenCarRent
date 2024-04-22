c6b496569d2d4db4a78492174fba85fd7562b892
Rubén <rsacri27@hotmail.com>
2024-04-22 16:19:09 +0200

    Crear VehiculoController

--- app/Http/Controllers/VehiculoController.php
+++ app/Http/Controllers/VehiculoController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Vehiculo;
 
 class VehiculoController extends Controller
 {
-    //
+    /**
+     * Muestra la página con los detalles del vehiculo
+     */
+    public function show($id)
+    {
+        $vehiculo = Vehiculo::findOrFail($id);
+        return view("public/vehiculo-show", ["vehiculo" => $vehiculo]);
+    }
+    /**
+     * Muestra la lista de vehiculos en la base de datos
+     */
+    public function index()
+    {
+        return view("empleado.vehiculo-index", ["vehiculo" => Vehiculo::paginate(10)]);
+    }
+
+
+   
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\VehiculoController;
 
 Route::view('/', "public/home")
     ->name('home');
@@ -15,3 +16,31 @@ Route::view("/condiciones", "public/condiciones")
 
 Route::view("/contacto", "public/contacto")
     ->name('contacto');
+
+
+    Route::get("/catalogo", [VehiculoController::class, "search"])
+    ->name('catalogo');
+
+Route::post("/catalogo/libro", [AlquilerController::class, "store"])
+    ->name('alquiler.store')->middleware("can:doesntHaveNoleggio");
+
+Route::get("/catalogo/vehiculo-{id}", [VehiculoController::class, "show"])
+    ->name('vehiculo.show');
+
+
+
+Route::view('login', "public/login")
+    ->name('login')->middleware("guest");
+
+Route::post('login', [UsuarioController::class, "authenticate"])
+    ->middleware("guest");;
+
+Route::view('register', "public/register")
+    ->name('register')->middleware("guest");
+
+Route::post('register', [ClienteController::class, "store"])
+    ->middleware("guest");;
+
+Route::get('/logout', [UsuarioController::class, 'logout'])
+    ->name('logout')->middleware("auth");
+
