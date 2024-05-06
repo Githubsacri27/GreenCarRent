b51430805599ee5bd83434210ba6f5fbf4d4506f
Rubén <rsacri27@hotmail.com>
2024-05-06 09:43:17 +0200

    refactorizar

--- app/Http/Controllers/StorageController.php
+++ app/Http/Controllers/StorageController.php
@@ -5,39 +5,37 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\File;
 
 /**
- * Controlador para la gestión de almacenamiento de archivos.
+ * Controlador para guardar archivos.
  * Proporciona métodos para almacenar, actualizar y eliminar imágenes,
  * así como para eliminar directorios.
  */
 class StorageController extends Controller
 {
     /**
-     * Guarda una imagen en un directorio especificado.
+     * Guarda una imagen.
      *
      * @param \Illuminate\Http\UploadedFile $file El archivo de imagen a guardar.
-     * @param string $name El nombre para el archivo de imagen.
+     * @param string $name El nombre para el archivo.
      * @param string $dir El directorio donde se guardará la imagen.
      *
-     * @return string La ruta relativa de la imagen guardada.
+     * @return string La ruta relativa de la imagen.
      */
-    public static function storeImage($file, $name, $dir)
-    {
+    public static function storeImage($file, $name, $dir){
         $filename = $name . "." . $file->getClientOriginalExtension();
         $foto = $file->storeAs($dir, $filename, 'public');
         return "storage/" . $foto;
     }
 
     /**
-     * Actualiza una imagen existente en un directorio especificado.
+     * Actualiza una imagen.
      *
      * @param \Illuminate\Http\UploadedFile $file El nuevo archivo de imagen.
-     * @param string $name El nombre para el archivo de imagen.
-     * @param string $dir El directorio donde se almacenará la nueva imagen.
+     * @param string $name El nombre para el archivo.
+     * @param string $dir El directorio donde se guarda la nueva imagen.
      *
-     * @return string La ruta relativa de la imagen actualizada.
+     * @return string La ruta.
      */
-    public static function updateImage($file, $name, $dir)
-    {
+    public static function updateImage($file, $name, $dir){
         self::findAndDeleteImage($name, $dir);
         return self::storeImage($file, $name, $dir);
     }
--- public/js/catalogo.js
+++ public/js/catalogo.js
@@ -1,174 +1,40 @@
-// Catálogo
-window.addEventListener('load', function() {
-    const filterButton = document.getElementById('filterButton');
-    const toggleFilter = document.getElementById('toggle-filter');
-
-    // Si el botón de filtro existe, añade un evento de clic
-    if (filterButton) {
-        filterButton.addEventListener('click', function() {
-            // Alterna la visibilidad del filtro
-            toggleFilter.style.display = toggleFilter.style.display === 'none' ? 'block' : 'none';
-        });
-    }
-});
-
-// Entrada de rango doble
-window.addEventListener('load', function() {
-    const rangeInput = document.querySelectorAll('.range-input input');
-    const priceInput = document.querySelectorAll('.price-input input');
-    const progress = document.querySelector('.slider .progress');
-
-    // Paso para el deslizador de rango doble
-    let step = 50;
-
-    // Tamaño inicial del elemento de progreso
-    function setMinDefaultValue(min) {
-        rangeInput[0].value = min;
-        priceInput[0].value = min;
-        progress.style.left = ((min / rangeInput[0].max) * 100) + '%';
-    }
-
-    function setMaxDefaultValue(max) {
-        rangeInput[1].value = max;
-        priceInput[1].value = max;
-        progress.style.right = 100 - (max / rangeInput[1].max) * 100 + '%';
-    }
-
-    // Establece valores por defecto si los inputs de precio están vacíos
-    if (priceInput[0].value === '' && priceInput[1].value === '') {
-        setMinDefaultValue(0);
-        setMaxDefaultValue(5000);
-    } else {
-        setMinDefaultValue(priceInput[0].value);
-        setMaxDefaultValue(priceInput[1].value);
-    }
-
-    // Escucha los cambios en los inputs numéricos y actualiza el deslizador y los valores
-    priceInput.forEach(input => {
-        input.addEventListener('input', function(e) {
-            let minPrice = priceInput[0].valueAsNumber;
-            let maxPrice = priceInput[1].valueAsNumber;
-
-            if (minPrice >= 0 && maxPrice > 0) {
-                if ((maxPrice - minPrice >= step) && maxPrice <= rangeInput[1].max) {
-                    if (e.target.className === 'input-min') {
-                        rangeInput[0].value = minPrice;
-                        progress.style.left = ((minPrice / rangeInput[0].max) * 100) + '%';
-                    } else if (e.target.className === 'input-max') {
-                        rangeInput[1].value = maxPrice;
-                        progress.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + '%';
-                    }
-                }
-            }
-        });
-    });
-
-    // Escucha los cambios en los deslizadores y actualiza los inputs de precio y el progreso
-    rangeInput.forEach(input => {
-        input.addEventListener('input', function(e) {
-            let minVal = rangeInput[0].valueAsNumber;
-            let maxVal = rangeInput[1].valueAsNumber;
-
-            if ((maxVal - minVal) < step) {
-                if (e.target.className === 'range-min') {
-                    rangeInput[0].value = maxVal - step;
-                } else {
-                    rangeInput[1].value = minVal + step;
-                }
-            } else {
-                priceInput[0].value = minVal;
-                priceInput[1].value = maxVal;
-                progress.style.left = ((minVal / rangeInput[0].max) * 100) + '%';
-                progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + '%';
-            }
-        });
-    });
-
-    // Escucha el evento "focusout" en el input de precio mínimo y establece valores por defecto
-    document.getElementById('priceMin').addEventListener('focusout', function() {
-        let minPrice = priceInput[0].valueAsNumber;
-        let maxPrice = priceInput[1].valueAsNumber;
-
-        if (maxPrice - minPrice <= step || minPrice < 0 || esNaN(minPrice)) {
-            setMinDefaultValue(0);
-        }
-    });
-
-    // Escucha el evento "focusout" en el input de precio máximo y establece valores por defecto
-    document.getElementById('priceMax').addEventListener('focusout', function() {
-        let minPrice = priceInput[0].valueAsNumber;
-        let maxPrice = priceInput[1].valueAsNumber;
-
-        if (maxPrice - minPrice <= step || maxPrice > rangeInput[1].max || esNaN(maxPrice)) {
-            setMaxDefaultValue(5000);
-        }
-    });
-});
-
-// Entrada de fechas
-
-// Función para obtener el siguiente día de la semana
-function getNextDay(day) {
-    let newDay = new Date(day.getTime());
-    newDay.setDate(newDay.getDate() + 1);
-    return newDay;
-}
-
-// Formato de salida de la fecha como YYYY-mm-dd
-function dateToString(date) {
-    let string = date.getFullYear() + '-';
-    let month = date.getMonth() + 1;
-    let day = date.getDate();
-
-    if (month < 10) {
-        string += '0' + month + '-';
-    } else {
-        string += month + '-';
-    }
-
-    if (day < 10) {
-        string += '0' + day;
-    } else {
-        string += day;
-    }
-
-    return string;
-}
-
-// Añade años a una fecha
-function addYear(date, year) {
-    let newDate = new Date(date.getTime());
-    newDate.setFullYear(newDate.getFullYear() + year);
-    return newDate;
-}
-
 // Escucha el evento de carga para inicializar los valores de los inputs de fecha
 window.addEventListener('load', function() {
     let today = new Date();
     let nextDay = getNextDay(today);
 
+    // Obtiene los elementos de entrada de fecha
     let fechaRecogida = document.getElementById('fechaRecogida');
     let fechaEntrega = document.getElementById('fechaEntrega');
 
-    // Establece atributos mínimos y máximos para fechaRecogida
-    fechaRecogida.min = dateToString(nextDay);
-    let OneYearAfterToday = addYear(today, 1);
-    fechaRecogida.max = dateToString(OneYearAfterToday);
+    // Si `fechaRecogida` existe en el DOM, establece los atributos `min` y `max`
+    if (fechaRecogida) {
+        // Establece atributos mínimos y máximos para `fechaRecogida`
+        fechaRecogida.min = dateToString(nextDay);
+        let oneYearAfterToday = addYear(today, 1);
+        fechaRecogida.max = dateToString(oneYearAfterToday);
 
-    // Establece atributos mínimos y máximos para fechaEntrega
-    let nextNextDay = getNextDay(nextDay);
-    fechaEntrega.min = dateToString(nextNextDay);
-    let ThreeYearAfterToday = addYear(today, 3);
-    fechaEntrega.max = dateToString(ThreeYearAfterToday);
+        // Escucha el evento de cambio en `fechaRecogida` y actualiza `fechaEntrega`
+        fechaRecogida.addEventListener('change', function() {
+            let nextDay = getNextDay(new Date(fechaRecogida.value));
+            if (fechaEntrega) {
+                fechaEntrega.min = dateToString(nextDay);
+                let fechaEntregaValue = new Date(fechaEntrega.value);
+                
+                // Si `fechaEntrega` es menor que `fechaRecogida`, ajusta `fechaEntrega`
+                if (nextDay > fechaEntregaValue) {
+                    fechaEntrega.value = dateToString(nextDay);
+                }
+            }
+        });
+    }
 
-    // Escucha el evento de cambio en fechaRecogida y actualiza fechaEntrega
-    fechaRecogida.addEventListener('change', function() {
-        let nextDay = getNextDay(new Date(fechaRecogida.value));
-        fechaEntrega.min = dateToString(nextDay);
-        let fechaEntregaValue = new Date(fechaEntrega.value);
-
-        if (nextDay > fechaEntregaValue) {
-            fechaEntrega.value = dateToString(nextDay);
-        }
-    });
+    // Si `fechaEntrega` existe en el DOM, establece los atributos `min` y `max`
+    if (fechaEntrega) {
+        // Establece atributos mínimos y máximos para `fechaEntrega`
+        let nextNextDay = getNextDay(nextDay);
+        fechaEntrega.min = dateToString(nextNextDay);
+        let threeYearsAfterToday = addYear(today, 3);
+        fechaEntrega.max = dateToString(threeYearsAfterToday);
+    }
 });
