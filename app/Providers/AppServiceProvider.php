52feb463fac93135b94d256d8b156b84f8aee737
Rubén <rsacri27@hotmail.com>
2024-05-06 13:06:46 +0200

    documentar politica de roles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,11 +7,12 @@ use App\Models\Alquiler;
 use App\Models\Usuario;
 use Illuminate\Support\Facades\Gate;
 
-
-class AppServiceProvider extends ServiceProvider
-{
+/**
+ * La Política de Roles de autenticación de la aplicación.
+ */
+class AppServiceProvider extends ServiceProvider{
     /**
-     * The policy mappings for the application.
+     * Las políticas de la aplicación.
      *
      * @var array<class-string, class-string>
      */
@@ -19,25 +20,33 @@ class AppServiceProvider extends ServiceProvider
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
+    /**
+     * Inicializador de autenticación.
+     */
     public function boot(){
         $this->registerPolicies();
 
+        // Define la política para verificar si el usuario es un admin.
         Gate::define('isAdmin', function(Usuario $usuario) {
             return $usuario->utenteable_type == 'Admin';
         });
 
+        // Define la política para verificar si el usuario es un empleado.
         Gate::define('isEmpleado', function(Usuario $usuario) {
             return $usuario->utenteable_type == 'App\Models\Empleado';
         });
 
+        // Define la política para verificar si el usuario es un empleado o administrador.
         Gate::define('isEmpleadoOrAdmin', function() {
             return Gate::allows("isEmpleado") or Gate::allows("isAdmin");
         });
 
+        // Define la política para verificar si el usuario es un cliente.
         Gate::define('isClient', function(Usuario $usuario) {
             return $usuario->utenteable_type == 'App\Models\Cliente';
         });
 
+        // Define la política para verificar si el usuario no tiene alquiler activo.
         Gate::define('doesntHaveAlquiler', function(Usuario $usuario) {
             return Gate::allows("isClient") and !Alquiler::where("clienteID", $usuario->utenteable_id)->where("activo", true)->exists();
         });
